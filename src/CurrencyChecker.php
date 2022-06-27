eded157e6e772cd0a49cafb6636f16b5aac94eb4
osteel <yannickchenot@gmail.com>
2022-06-27 14:43:25 +0100

    fixed test

--- src/CurrencyChecker.php
+++ src/CurrencyChecker.php
@@ -6,14 +6,20 @@ use Money\Money;
 
 class CurrencyChecker
 {
-	public function isSame(Money ...$amounts): bool
-	{
-		if (count($amounts) === 1) {
-			return true;
-		}
+    public function isSame(Money ...$amounts): bool
+    {
+        if (count($amounts) === 1) {
+            return true;
+        }
 
-		$first = array_shift($amounts);
+        $first = array_shift($amounts);
 
-		return $first->isSameCurrency(...$amounts);
-	}
+        foreach ($amounts as $amount) {
+            if (! $first->isSameCurrency($amount)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
